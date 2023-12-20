65611dd45749ce1b32771370a13292dd5da1768a
gersar08 <nagerman26@gmail.com>
2023-12-20 13:41:23 -0600

    Refactor Header component and add logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { ArrowLeftOnRectangleIcon, HomeIcon} from "@heroicons/react/24/outline";
+import {
+  ArrowLeftOnRectangleIcon,
+  HomeIcon,
+} from "@heroicons/react/24/outline";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Header = () => {
@@ -7,43 +10,55 @@ const Header = () => {
   const location = useLocation();
 
   const handleLogout = async () => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      // Redirige al usuario a la página de inicio de sesión si no hay token
+      navigate("/login");
+      return;
+    }
+
     // Haz una solicitud a la ruta /logout de tu API
-    const response = await fetch('https://rocky-dawn-84773-5951dec09d0b.herokuapp.com/api/logout', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
+    const response = await fetch(
+      "https://rocky-dawn-84773-5951dec09d0b.herokuapp.com/api/logout",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
       }
-    });
+    );
 
     if (response.ok) {
       // Borra la información de la sesión del usuario
-      localStorage.removeItem('token');
+      localStorage.removeItem("token");
       // Redirige al usuario a la página de inicio de sesión
-      navigate('/login');
+      navigate("/login");
     } else {
-      console.error('Logout failed');
+      console.error("Logout failed");
     }
   };
-
   return (
     <header className="flex items-center justify-between p-4 bg-sky-500">
       <div className="flex items-center">
         <Link to={"/admin-dashboard"}>
-        <h1 className="text-xl font-bold">Pure life care</h1>
+          <h1 className="text-xl font-bold">Pure life care</h1>
         </Link>
       </div>
       <div className="flex items-center">
-      {location.pathname !== '/login' && (
-        <>
-          {/*<button>
+        {location.pathname !== "/login" && (
+          <>
+            {/*<button>
             <BellAlertIcon className="h-7 w-7 mr-6" />
           </button>*/}
-          <Link to={'/admin-dashboard'} className="h-7 w-7 mr-6"><HomeIcon /> </Link>
-        </>
-      )}
+            <Link to={"/admin-dashboard"} className="h-7 w-7 mr-6">
+              <HomeIcon />{" "}
+            </Link>
+          </>
+        )}
         <button id="logout" onClick={handleLogout}>
-        <ArrowLeftOnRectangleIcon className="h-7 w-7" />
+          <ArrowLeftOnRectangleIcon className="h-7 w-7" />
         </button>
       </div>
     </header>
