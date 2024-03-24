68a3e367f69e1fe39d368a536cc2d9ffd8d5514a
gersar08 <nagerman26@gmail.com>
2024-03-23 22:20:40 -0600

    Nothing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  /** @type {import('tailwindcss').Config} */
+  module.exports = {
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
 
